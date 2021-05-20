31455e7358a3db7e2dcbd6b7da457ad536c08a98
gannacon <cwgannaway@gmail.com>
2021-05-19 23:06:39 -0500

    Working inputs in index and employee

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-// INCLUDE THE FILES TO START THIS CODE.
 const inquirer = require("inquirer");
-const fs = require("fs");
-
-const Employee = require("./lib/Employee")
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-
+const Employee = require("./lib/Employee.js");
+// const Engineer = require("./lib/Engineer.js");
+// const Intern = require("./lib/Intern.js");
+const Mangager = require("./lib/Manager.js");
 
 //Initialize the new Employee Object
-const employee = new.Employee();
+const employee = new Employee();
 
-// Start by getting the name of the manager
-employee.getName();
+// TODO: Create a function to initialize app
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "start",
+        message: "Would you like to log a new employee? ",
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then((Start) => {
+      if (Start.start === "Yes") {
+        console.log("Great!");
+        employee.getRole();
+      } else {
+        return;
+      }
+    });
+}
+
+// Function call to initialize app
+init();
